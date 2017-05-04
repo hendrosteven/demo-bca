a94713ea3d9381cec7e3778994619b28170a5544
Hendro Steven <hendro.steven@gmail.com>
2017-05-04 16:43:06 +0800

    Initial commit

---
+++ src/main/java/com/DemoBcaAppApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBcaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBcaAppApplication.class, args);
+	}
+}
